ce999907254d0306c5549467716a89e6fd6102d6
covalt-ta <taku.covayashi@gmail.com>
2020-06-25 21:55:51 +0900

    投稿一覧の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'boards#index'
+  get 'boards', to: 'boards#index'
+  # /boardsにリクエストが来た場合、BoardsControllerのindexアクションが呼ばれる
+  # そしてviewsディレクトリのboards/index.htmlが表示される
+  get 'boards/new', to: 'boards#new'
+  post 'boards', to: 'boards#create'
 end
