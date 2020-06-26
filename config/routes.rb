91fed8039805a06fa5a0c1276623d75f195ce6b3
covalt-ta <taku.covayashi@gmail.com>
2020-06-26 13:40:04 +0900

    投稿一覧・詳細・新規作成のアクション、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
   # /boardsにリクエストが来た場合、BoardsControllerのindexアクションが呼ばれる
   # そしてviewsディレクトリのboards/index.htmlが表示される
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
+  resources :boards, only: [:index, :new, :create, :show]
 end
