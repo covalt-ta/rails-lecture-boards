afdc605fdf190dd92812cbf9cf13de7b0722d3dc
covalt-ta <taku.covayashi@gmail.com>
2020-06-30 20:57:22 +0900

    commentモデル、コントローラの作成、view修正中

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,33 +1,51 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
-    @boards = Board.all
+    @boards = Board.page(params[:page])
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
-    Board.create(board_params)
+    board = Board.new(board_params)
+    if board.save
+      flash[:notice] = "「#{board.title}」の掲示板の作成に成功しました"
+      redirect_to board
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+        }
+    end
   end
 
   def show
-    @board = Board.find(params[:id])
+    @comment = @board.comments.new
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-    redirect_to board #redirect_toの引数にオブジェクトを渡すと詳細へリダイレクトする
+    @board.update(board_params)
+    redirect_to @board #redirect_toの引数にオブジェクトを渡すと詳細へリダイレクトする
+  end
+
+  def destroy
+    @board.delete
+
+    redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示版が削除されました"}
   end
 
   private
-
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # /boardsにリクエストが来た場合、BoardsControllerのindexアクションが呼ばれる
   # そしてviewsディレクトリのboards/index.htmlが表示される
   resources :boards
+  resources :comments, only: %i[create destroy]
 end
